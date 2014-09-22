fb9680865dace025d2347e19f4f51eda4be217be
Nick Otero <notero87@gmail.com>
2014-09-22 02:32:42 -0400

    Added move base functionality to the zig zag demo

--- turtlebot_dev/scripts/zig_zag_demo.py
+++ turtlebot_dev/scripts/zig_zag_demo.py
@@ -5,7 +5,7 @@
 #the poses through the service WaypointServerService
 import rospy
 from std_msgs.msg import Header
-from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
+from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 import tf
 
@@ -15,59 +15,89 @@ class ZigZag:
 	#Creates the service, begins listening to the robot pose, and then spins
 	def __init__(self):
 		#Globals
+		self.waypoints = []
+		self.goal = PoseStamped()
 
 		#Subscribers
-		#self.pose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.pose_callback)
-
 		self.listener = tf.TransformListener()
 
 		#Publishers
-		self.map_pub = rospy.Publisher('/zig_zag_demo/empty_map', OccupancyGrid)
-		self.create_empty_map()
+		self.goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
+
+		#Init Functions
+		self.init_waypoints()
 
 		rospy.on_shutdown(self.shutdown)
 		self.run()
 
 	def getTransform(self):
-		#try:
-		(trans, rot) = self.listener.lookupTransform('ar_marker_0', 'camera_rgb_optical_frame', rospy.Time(0))
-			#rospy.loginfo(trans)
-			#rospy.loginfo(rot)
-		#except:
-		#	rospy.loginfo("Ouch!")
+		try:
+			(trans, rot) = self.listener.lookupTransform('ar_marker_0', 'base_link', rospy.Time(0))
+			return (trans.x, trans.y)
+		except:
+			rospy.loginfo("Ouch!")
+
+	def init_waypoints(self):
+		self.waypoints = []
+
+		x0 = 0.8
+		y0 = 1.5
+		dx = 0.2
+		dy = 1
+
+		for i in range(16):
+			x = 0
+			y = 0
+
+			counter = i % 4
+
+			if counter == 0:
+				x = x0
+				y = y0
+			elif counter == 1:
+				x = x0
+				y = y0 - dy
+			elif counter == 2:
+				x0 -= dx
+				x = x0
+				y = y0 - dy
+			elif counter == 3:
+				x = x0
+				y = y0
+				x0 -= dx
+
+			pose = PoseStamped()
+			header = Header()
+			header.frame_id = "ar_marker_0"
+
+			pose.header = header
+			pose.pose.position.x = -x
+			pose.pose.position.z = y
+
+			self.waypoints.append(pose)
+
+	def areWeThereYet(self):
+		tol = 0.05
+
+		curXY = self.getTransform()
+		goalXY = (self.goal.pose.position.x, self.goal.pose.position.y)
+
+		if (curXY[0] - goalXY[0]) < tol and (curXY[1] - goalXY[1]) < tol:
+			return True
+		return False
+
+	def nextGoal(self):
+		if not self.waypoints == []:
+			goal = self.waypoints.pop(0)
+			self.goal_pub.publish(goal)
+			rospy.loginfo("Published new goal!")
 
 	def run(self):
+		self.nextGoal()
 		while not rospy.is_shutdown():
-			self.getTransform()
+			if self.areWeThereYet:
+				self.nextGoal()
 
-	def create_empty_map(self):
-		rospy.loginfo("Creating empty map")
-
-		header = Header()
-		header.frame_id = 'map'
-
-		meta_data = MapMetaData()
-		meta_data.resolution = .2
-		meta_data.width = 20
-		meta_data.height = 20
-		meta_data.origin = Pose()
-
-		rospy.loginfo("Meta Data created")
-
-		data = [0 for i in range(200)]
-
-		rospy.loginfo("Data created")
-
-		empty_map = OccupancyGrid()
-		empty_map.header = header
-		empty_map.info = meta_data
-		empty_map.data = data
-
-		rospy.loginfo("Created empty map:\n")
-		print empty_map
-
-		self.map_pub.publish(empty_map)
-		rospy.loginfo("Map Published!")
 
 	def shutdown(self):
 		pass
