d30e7f0f29b68ca1639bee0bb68fb5cdb3b6d4c8
Nick Otero <notero87@gmail.com>
2014-09-25 00:20:01 -0400

    Edited the zig zag demo python file to include a pose array and fix bugs.  Also added some nodes and launches to the zig zag demo launch file

--- turtlebot_dev/scripts/zig_zag_demo.py
+++ turtlebot_dev/scripts/zig_zag_demo.py
@@ -5,7 +5,7 @@
 #the poses through the service WaypointServerService
 import rospy
 from std_msgs.msg import Header
-from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped
+from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped, PoseArray
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 import tf
 
@@ -23,22 +23,24 @@ class ZigZag:
 
 		#Publishers
 		self.goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
+		self.pose_array_pub = rospy.Publisher('/zig_zag_demo/pose_array', PoseArray)
 
 		#Init Functions
 		self.init_waypoints()
 
-		rospy.on_shutdown(self.shutdown)
 		self.run()
 
 	def getTransform(self):
 		try:
 			(trans, rot) = self.listener.lookupTransform('ar_marker_0', 'base_link', rospy.Time(0))
 			return (trans.x, trans.y)
-		except:
-			rospy.loginfo("Ouch!")
+		except Exception as e:
+			rospy.loginfo("Ouch! " + str(e))
 
 	def init_waypoints(self):
 		self.waypoints = []
+		self.pose_array = PoseArray()
+		pose_array_list = []
 
 		x0 = 0.8
 		y0 = 1.5
@@ -66,15 +68,25 @@ class ZigZag:
 				y = y0
 				x0 -= dx
 
-			pose = PoseStamped()
+			pose_msg = Pose()
+			pose_stamp_msg = PoseStamped()
 			header = Header()
 			header.frame_id = "ar_marker_0"
 
-			pose.header = header
-			pose.pose.position.x = -x
-			pose.pose.position.z = y
+			pose_stamp_msg.header = header
+			pose_msg.position.x = -x
+			pose_msg.position.z = y
+
+			pose_stamp_msg.pose = pose_msge
+			pose_array_list.append(pose_msg)
 
 			self.waypoints.append(pose)
+
+		self.pose_array.poses = pose_array_list
+
+		self.pose_array_pub.publish(self.pose_array)
+
+		rospy.loginfo("Waypoints initialized and published!")
 
 	def areWeThereYet(self):
 		tol = 0.05
@@ -83,13 +95,14 @@ class ZigZag:
 		goalXY = (self.goal.pose.position.x, self.goal.pose.position.y)
 
 		if (curXY[0] - goalXY[0]) < tol and (curXY[1] - goalXY[1]) < tol:
+			rospy.loginfo("Goal reached!")
 			return True
 		return False
 
 	def nextGoal(self):
 		if not self.waypoints == []:
-			goal = self.waypoints.pop(0)
-			self.goal_pub.publish(goal)
+			self.goal = self.waypoints.pop(0)
+			self.goal_pub.publish(self.goal)
 			rospy.loginfo("Published new goal!")
 
 	def run(self):
@@ -97,10 +110,6 @@ class ZigZag:
 		while not rospy.is_shutdown():
 			if self.areWeThereYet:
 				self.nextGoal()
-
-
-	def shutdown(self):
-		pass
 
 if __name__=="__main__":
 	try:
