7a27345293bb634637f9030d4fdbcefca8075292
Nick Otero <notero87@gmail.com>
2014-09-28 01:45:15 -0400

    Heavily edited the tf practice file

--- turtlebot_dev/scripts/tf_practice.py
+++ turtlebot_dev/scripts/tf_practice.py
@@ -26,70 +26,141 @@ class Sanity:
 
         self.run()
 
-    def transformPose(self, pose_msg, target_frame):
-        from_frame = pose_msg.header.frame_id
+    def quaternion_matrix(quaternion): 
+        """Return homogeneous rotation matrix from quaternion. 
 
-        direction = (pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z)
-        from_quat = (pose_msg.pose.orientation.x, pose_msg.pose.orientation.y, pose_msg.pose.orientation.z, pose_msg.pose.orientation.w)
-        M = numpy.identity(4) 
-        M[:3, 3] = direction[:3]
+        >>> R = quaternion_matrix([0.06146124, 0, 0, 0.99810947]) 
+        >>> numpy.allclose(R, rotation_matrix(0.123, (1, 0, 0))) 
+        True 
 
-        EPS = numpy.finfo(float).eps * 4.0 
-
-        q = numpy.array(from_quat[:4], dtype=numpy.float64, copy=True) 
+        """ 
+        q = numpy.array(quaternion[:4], dtype=numpy.float64, copy=True) 
         nq = numpy.dot(q, q) 
-        if nq < EPS: 
-            N =  numpy.identity(4) 
-        else:
-            q *= math.sqrt(2.0 / nq) 
-            q = numpy.outer(q, q) 
-            N =  numpy.array(( 
-            (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], 0.0), 
-            (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], 0.0), 
-            (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], 0.0), 
-            (                0.0,                 0.0,                 0.0, 1.0) 
-            ), dtype=numpy.float64) 
+        if nq < _EPS: 
+          return numpy.identity(4) 
+        q *= math.sqrt(2.0 / nq) 
+        q = numpy.outer(q, q) 
+        return numpy.array(( 
+          (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], 0.0), 
+          (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], 0.0), 
+          (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], 0.0), 
+          (                0.0,                 0.0,                 0.0, 1.0) 
+          ), dtype=numpy.float64)
 
+    def translation_matrix(direction): 
+        """Return matrix to translate by direction vector. 
 
-        from_pose_con = numpy.dot(M, N)
+        >>> v = numpy.random.random(3) - 0.5 
+        >>> numpy.allclose(v, translation_matrix(v)[:3, 3]) 
+        True 
 
-        self.listener.waitForTransform(from_frame, target_frame, rospy.Time(0), rospy.Duration(20))
-        transform = self.listener.lookupTransform(from_frame, target_frame, rospy.Time(0))
+        """ 
+        M = numpy.identity(4) 
+        M[:3, 3] = direction[:3] 
+        return M 
 
-        tfMatrix = numpy.dot(transform[0], transform[1])
+    def translation_from_matrix(matrix): 
+        """Return translation vector from translation matrix. 
+    
+        >>> v0 = numpy.random.random(3) - 0.5 
+        >>> v1 = translation_from_matrix(translation_matrix(v0)) 
+        >>> numpy.allclose(v0, v1) 
+        True 
+    
+        """ 
+        return numpy.array(matrix, copy=False)[:3, 3].copy() 
 
-        target_pose_matrix = numpy.dot(tfMatrix, from_pose_con)
+    def quaternion_from_matrix(matrix): 
+        """Return quaternion from rotation matrix. 
 
-        target_xyz = tuple(numpy.array(target_pose_matrix, copy=False)[:3, 3].copy())[:3]
+        >>> R = rotation_matrix(0.123, (1, 2, 3)) 
+        >>> q = quaternion_from_matrix(R) 
+        >>> numpy.allclose(q, [0.0164262, 0.0328524, 0.0492786, 0.9981095]) 
+        True 
 
-        p = numpy.empty((4, ), dtype=numpy.float64) 
+        """ 
+        q = numpy.empty((4, ), dtype=numpy.float64) 
         M = numpy.array(matrix, dtype=numpy.float64, copy=False)[:4, :4] 
         t = numpy.trace(M) 
         if t > M[3, 3]: 
-            p[3] = t 
-            p[2] = M[1, 0] - M[0, 1] 
-            p[1] = M[0, 2] - M[2, 0] 
-            p[0] = M[2, 1] - M[1, 2] 
+          q[3] = t 
+          q[2] = M[1, 0] - M[0, 1] 
+          q[1] = M[0, 2] - M[2, 0] 
+          q[0] = M[2, 1] - M[1, 2] 
         else: 
-            i, j, k = 0, 1, 2 
-            if M[1, 1] > M[0, 0]: 
-                i, j, k = 1, 2, 0 
-            if M[2, 2] > M[i, i]: 
-                i, j, k = 2, 0, 1 
-            t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3] 
-            p[i] = t 
-            p[j] = M[i, j] + M[j, i] 
-            p[k] = M[k, i] + M[i, k] 
-            p[3] = M[k, j] - M[j, k] 
-        p *= 0.5 / math.sprt(t * M[3, 3]) 
-        
-        new_quat = tuple(p)
+          i, j, k = 0, 1, 2 
+          if M[1, 1] > M[0, 0]: 
+              i, j, k = 1, 2, 0 
+          if M[2, 2] > M[i, i]: 
+              i, j, k = 2, 0, 1 
+          t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3] 
+          q[i] = t 
+          q[j] = M[i, j] + M[j, i] 
+          q[k] = M[k, i] + M[i, k] 
+          q[3] = M[k, j] - M[j, k] 
+        q *= 0.5 / math.sqrt(t * M[3, 3]) 
+        return q 
 
+    def fromTranslationRotation(self, translation, rotation): 
+        """ 
+        :param translation: translation expressed as a tuple (x,y,z) 
+        :param rotation: rotation quaternion expressed as a tuple (x,y,z,w) 
+        :return: a :class:`numpy.matrix` 4x4 representation of the transform 
+        :raises: any of the exceptions that :meth:`~tf.Transformer.lookupTransform` can raise 
+
+        Converts a transformation from :class:`tf.Transformer` into a representation as a 4x4 matrix. 
+        """ 
+
+        return numpy.dot(self.translation_matrix(translation), self.quaternion_matrix(rotation))
+
+    def xyz_to_mat44(pos): 
+        return self.translation_matrix((pos.x, pos.y, pos.z)) 
+
+    def xyzw_to_mat44(ori): 
+        return self.quaternion_matrix((ori.x, ori.y, ori.z, ori.w))
+
+    def asMatrix(self, target_frame, hdr): 
+        """ 
+        :param target_frame: the tf target frame, a string 
+        :param hdr: a message header 
+        :return: a :class:`numpy.matrix` 4x4 representation of the transform 
+        :raises: any of the exceptions that :meth:`~tf.Transformer.lookupTransform` can raise 
+
+        Uses :meth:`lookupTransform` to look up the transform for ROS message header hdr to frame 
+        target_frame, and returns the transform as a :class:`numpy.matrix` 
+        4x4. 
+        """ 
+        self.listener.waitForTransform(target_frame, hdr.frame_id, hdr.stamp, rospy.Duration(40))
+        translation,rotation = self.listener.lookupTransform(target_frame, hdr.frame_id, hdr.stamp) 
+        return self.fromTranslationRotation(translation, rotation) 
+
+    def transformPose(self, target_frame, ps): 
+        """
+        :param target_frame: the tf target frame, a string 
+        :param ps: the geometry_msgs.msg.PoseStamped message 
+        :return: new geometry_msgs.msg.PoseStamped message, in frame target_frame 
+        :raises: any of the exceptions that :meth:`~tf.Transformer.lookupTransform` can raise 
+
+        Transforms a geometry_msgs PoseStamped message to frame target_frame, returns a new PoseStamped message. 
+        """ 
+        # mat44 is frame-to-frame transform as a 4x4 
+        mat44 = self.asMatrix(target_frame, ps.header) 
+
+        # pose44 is the given pose as a 4x4 
+        pose44 = numpy.dot(xyz_to_mat44(ps.pose.position), xyzw_to_mat44(ps.pose.orientation)) 
+
+        # txpose is the new pose in target_frame as a 4x4 
+        txpose = numpy.dot(mat44, pose44) 
+
+        # xyz and quat are txpose's position and orientation 
+        xyz = tuple(self.translation_from_matrix(txpose))[:3] 
+        quat = tuple(self.quaternion_from_matrix(txpose)) 
+
+        # assemble return value PoseStamped 
         r = geometry_msgs.msg.PoseStamped() 
-        r.header.stamp = pose_msg.header.stamp 
+        r.header.stamp = ps.header.stamp 
         r.header.frame_id = target_frame 
-        r.pose = geometry_msgs.msg.Pose(geometry_msgs.msg.Point(*target_xyz), geometry_msgs.msg.Quaternion(*new_quat))
-
+        r.pose = geometry_msgs.msg.Pose(geometry_msgs.msg.Point(*xyz), geometry_msgs.msg.Quaternion(*quat)) 
         return r
 
     def init_waypoints(self):
@@ -135,7 +206,7 @@ class Sanity:
 
             pose_stamp_msg.pose = pose_msg
 
-            test_pose = self.transformPose(pose_stamp_msg, "map")
+            test_pose = self.transformPose("map", pose_stamp_msg)
 
 
             pose_array_list.append(test)
