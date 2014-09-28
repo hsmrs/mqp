90e5a474902356f102301022e23da44b740285fa
trdesilva <trdesilva@wpi.edu>
2014-09-28 02:10:47 -0400

    Edited tf practice and got results on the transformation. Have not confirmed if they are correct or not.

--- turtlebot_dev/scripts/ar_latch.py
+++ turtlebot_dev/scripts/ar_latch.py
@@ -14,7 +14,7 @@ class ArLatch:
 
 	def __init__(self):
 		#Globals
-		self.lastTransform = None
+		self.transform = None
 
 		#Subscribers
 		self.listener = tf.TransformListener()
@@ -23,44 +23,39 @@ class ArLatch:
 		self.br = tf.TransformBroadcaster()
 
 		#Init Functions
+		while self.transform == None:
+			self.transform = self.getTransform("odom")
 
 		self.run()
 
 	def getTransform(self, frame):
 		try:
-			rospy.loginfo("Getting transform")
+			#rospy.loginfo("Getting transform")
 			(trans, rot) = self.listener.lookupTransform('ar_marker_0', frame, rospy.Time(0))
 			return (trans, rot)
 		except:
-			rospy.loginfo("Error getting transform")
+			#rospy.loginfo("Error getting transform")
 			return None
 
 	def sendTransform(self, frame):
 		try:
-			x = self.lastTransform[0][0]
-			y = self.lastTransform[0][1]
-			z = self.lastTransform[0][2]
-			roll = self.lastTransform[1][0]
-			pitch = self.lastTransform[1][1]
-			yaw = self.lastTransform[1][2]
+			x = self.transform[0][0]
+			y = self.transform[0][1]
+			z = self.transform[0][2]
+			roll = self.transform[1][0]
+			pitch = self.transform[1][1]
+			yaw = self.transform[1][2]
 
 			rospy.loginfo("Broadcasting transform!")
 			self.br.sendTransform((x, y, z), tf.transformations.quaternion_from_euler(roll, pitch, yaw), \
-				rospy.Time.now(), "ar_marker_0", frame)
+				rospy.Time.now(), "ar_marker_perm", frame)
 		except:
 			rospy.loginfo("Error sending transform")
 
 	def run(self):
 		while not rospy.is_shutdown():
-			transform = self.getTransform("odom")
-			if transform == None:
-				rospy.loginfo("No transform given, latching to last received transform")
-				self.sendTransform("odom")
-			else:
-				self.lastTransform = transform
-				rospy.loginfo("got transform " + str(self.lastTransform))
-				self.sendTransform("odom")
-			rospy.sleep(1)
+			self.sendTransform("odom")
+			rospy.sleep(.5)
 
 if __name__=="__main__":
 	try:
