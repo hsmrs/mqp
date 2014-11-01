da2867334f5ce9525efe4b630c9981fd48cc91d5
Nick Otero <notero87@gmail.com>
2014-11-01 16:53:12 -0400

    Created a branch for hermes development. Set up the directories and started two cpp files

---
+++ hermes/hermes_kinematics/src/forward_kinematics.cpp
@@ -0,0 +1,10 @@
+/****************************************************************************************
+*** File: forward_kinematics.cpp													  ***
+*** Author: HSMRS MQP Team															  ***
+*** Date: 11/01/2014																  ***
+***																					  ***
+*** Description: This file creates a ROS node to calculate the forward kinematics of  ***
+***              the Hermes robot.  It accepts twist messages for the rotation speeds ***
+***				 of the four wheels and outputs an Odometry message for the velocity  ***
+***              of the robot.														  ***
+****************************************************************************************/
---
+++ hermes/hermes_kinematics/src/inverse_kinematics.cpp
@@ -0,0 +1,11 @@
+/****************************************************************************************
+*** File: inverse_kinematics.cpp													  ***
+*** Author: HSMRS MQP Team															  ***
+*** Date: 11/01/2014																  ***
+***																					  ***
+*** Description: This file creates a ROS node to calculate the inverse kinematics of  ***
+***              the Hermes robot.  It accepts twist messages for the linear and 	  ***
+***				 rotation speeds of the robot and publishes twist messages for the    ***
+***				 rotation speeds of the four wheels of the robot.                     ***
+***              														  			  ***
+****************************************************************************************/
