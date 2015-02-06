4d66b66627370f82e3ab282f9acfa6d6a072dbd6
Nick Otero <notero87@gmail.com>
2015-02-06 01:54:45 -0500

    Fixed errors with parsing task from string

--- hsmrs_framework/src/tasks/GoToTask.cpp
+++ hsmrs_framework/src/tasks/GoToTask.cpp
@@ -29,13 +29,14 @@ GoToTask::GoToTask(std::string strDelimitedTask){
     	items.push_back(strDelimitedTask.substr(0, pos));
     	strDelimitedTask.erase(0, pos + delimiter.length());
 	}
+	items.push_back(strDelimitedTask);
 
     id = std::stoi(items[0]);
 	std::string paramList = items[2];
 
     std::vector<std::string> items2;
-    paramList.erase(0);
-    paramList.erase(paramList.end());
+    paramList.erase(0, 1);
+    paramList.pop_back();
 
     delimiter = ",";
 	pos = 0;
@@ -44,6 +45,7 @@ GoToTask::GoToTask(std::string strDelimitedTask){
     	items2.push_back(paramList.substr(0, pos));
     	paramList.erase(0, pos + delimiter.length());
 	}
+	items2.push_back(paramList);
 
 	std::vector<std::string> items3;
 	delimiter = "=";
@@ -53,6 +55,7 @@ GoToTask::GoToTask(std::string strDelimitedTask){
     	items3.push_back(items2[0].substr(0, pos));
     	items2[0].erase(0, pos + delimiter.length());
 	}
+	items3.push_back(items2[0]);
 
     double x = std::stoi(items3[1]);
 
@@ -63,12 +66,14 @@ GoToTask::GoToTask(std::string strDelimitedTask){
     	items4.push_back(items2[1].substr(0, pos));
     	items2[1].erase(0, pos + delimiter.length());
 	}
+	items4.push_back(items2[1]);
+
     double y = std::stoi(items4[1]);
 
     ROS_INFO("Getting owner list");
     std::string ownerList = items[4];
-    ownerList.erase(0);
-    ownerList.erase(paramList.end());
+    ownerList.erase(0, 1);
+    ownerList.pop_back();
 
     std::vector<std::string> items5;
     delimiter = ",";
@@ -78,6 +83,7 @@ GoToTask::GoToTask(std::string strDelimitedTask){
     	items5.push_back(ownerList.substr(0, pos));
     	ownerList.erase(0, pos + delimiter.length());
 	}
+	items5.push_back(ownerList);
 
     for (std::vector<std::string>::iterator it = items2.begin(); it != items2.end(); ++it){
      	std::string owner = *it;
