823ff0036fc07344ca88280b2d2c614894766615
thomas <trdesilva@wpi.edu>
2015-03-01 21:46:54 -0500

    added unlocks and try-catches to GoTo mutexes

--- thor/src/GoToBehavior.cpp
+++ thor/src/GoToBehavior.cpp
@@ -16,37 +16,77 @@ GoToBehavior::GoToBehavior(Robot* parent, geometry_msgs::Point goal, ros::NodeHa
 
 void GoToBehavior::execute(){
 	ROS_INFO("Sending goal");
-	boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
-	isExecuting = true;
-  	goalPub.publish(goalMsg);
+	try
+	{
+	    boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
+	    isExecuting = true;
+	    isExecutingLock.unlock();
+      	goalPub.publish(goalMsg);
+  	}
+  	catch(boost::lock_error& e)
+  	{
+  	    ROS_INFO("lock error %s", e.what());
+  	}
 }
 
 void GoToBehavior::resume(){
-    boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
-	isExecuting = true;
-	goalPub.publish(goalMsg);
+    try
+    {
+        boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
+	    isExecuting = true;
+	    isExecutingLock.unlock();
+	    goalPub.publish(goalMsg);
+	}
+  	catch(boost::lock_error& e)
+  	{
+  	    ROS_INFO("lock error %s", e.what());
+  	}
 }
 
 void GoToBehavior::pause(){
-    boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
-	isExecuting = false;
-	cancelPub.publish(cancelMsg);
+    try
+    {
+        boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
+	    isExecuting = false;
+	    isExecutingLock.unlock();
+	    cancelPub.publish(cancelMsg);
+    }
+  	catch(boost::lock_error& e)
+  	{
+  	    ROS_INFO("lock error %s", e.what());
+  	}
 }
 
 void GoToBehavior::stop(){
-    boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
-	isExecuting = false;
-	cancelPub.publish(cancelMsg);
+    try
+    {
+        boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
+	    isExecuting = false;
+	    isExecutingLock.unlock();
+	    cancelPub.publish(cancelMsg);
+    }
+  	catch(boost::lock_error& e)
+  	{
+  	    ROS_INFO("lock error %s", e.what());
+  	}
 }
 
 void GoToBehavior::progressCallback(const std_msgs::String::ConstPtr& msg){
 	std::string progress = msg->data;
-	boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
-	if (progress == "complete" && isExecuting){
-		isExecuting = false;
+	try
+	{
+	    boost::mutex::scoped_lock isExecutingLock(isExecutingMutex);
+	    if (progress == "complete" && isExecuting){
+		    isExecuting = false;
 		
-		//Tell thor task is complete.
-		ROS_INFO("GoToTask complete, calling cancelTask on %s", info.c_str());
-		progressPub.publish(*msg);
+		    //Tell thor task is complete.
+		    ROS_INFO("GoToTask complete, calling cancelTask on %s", info.c_str());
+		    progressPub.publish(*msg);
+	    }
+	    isExecutingLock.unlock();
 	}
+  	catch(boost::lock_error& e)
+  	{
+  	    ROS_INFO("lock error %s", e.what());
+  	}
 }
